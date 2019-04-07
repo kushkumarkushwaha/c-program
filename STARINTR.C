56b8df87b0189839b5e453caf805c22520d476fa
kushkumarkushwaha <45714184+kushkumarkushwaha@users.noreply.github.com>
2019-04-07 11:40:00 -0700

    update

---
+++ STARINTR.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int row,i,j;
+printf("enter number of row:");
+scanf("%d",&row);
+for(i=1;i<=row;i++)
+{
+for(j=1;j<=i;j++)
+{
+printf("* ");
+
+}
+printf("\n");
+}
+getch();
+}
